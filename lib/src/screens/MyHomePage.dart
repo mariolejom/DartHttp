2d660f690d73480cee4038218003859d4aa0582b
Mario Moreno <malejo279@gmail.com>
2020-05-23 18:14:49 -0500

    Carga de archivos iniciales

---
+++ lib/src/screens/MyHomePage.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: customContainer(),
+//      floatingActionButton: FloatingActionButton(
+//        onPressed: _incrementCounter,
+//        tooltip: 'Increment',
+//        child: Icon(Icons.add),
+//      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+  Widget customContainer(){
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      //color: Colors.red,
+      padding: EdgeInsets.all(16),
+      margin: EdgeInsets.all(16),
+      alignment: Alignment.center,
+      child: Text("Platzi".toUpperCase()),
+      decoration: BoxDecoration(
+        color: Colors.purple,
+        border: Border.all(
+          color: Colors.teal,
+          width: 5,
+        ),
+      ),
+      transform: Matrix4.rotationZ(.05),
+      constraints: BoxConstraints(maxWidth: 100),
+    );
+  }
+}
