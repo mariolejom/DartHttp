2a1979791cabfa24bee81de843d9f964fcc7f1e5
Mario Moreno <malejo279@gmail.com>
2020-05-24 19:34:43 -0500

    MyHomePage showing posts

--- lib/src/screens/MyHomePage.dart
+++ lib/src/screens/MyHomePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/src/api/api.dart';
 import 'package:flutterapp/src/models/photos.dart';
+import 'package:flutterapp/src/models/posts.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -19,7 +20,33 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(),
+      body: FutureBuilder<List<Posts>>(
+        future: api.getPosts(),
+        builder: (BuildContext context, AsyncSnapshot<List<Posts>> snapshot) {
+          if (snapshot.hasData &&
+              snapshot.connectionState == ConnectionState.done) {
+            return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index){
+                  return Card(
+                    child: ListTile(
+//                      leading: CircleAvatar(
+//                        radius: 30,
+//                        backgroundImage: NetworkImage(snapshot.data[index].title),
+//                      ),
+                      title: Text(snapshot.data[index].title),
+                      subtitle: Text(snapshot.data[index].body),
+                    ),
+                  );
+
+                });
+          } else {
+            return Center(
+              child: LinearProgressIndicator(),
+            );
+          }
+        },
+      ),
 //      floatingActionButton: FloatingActionButton(
 //        onPressed: _incrementCounter,
 //        tooltip: 'Increment',
